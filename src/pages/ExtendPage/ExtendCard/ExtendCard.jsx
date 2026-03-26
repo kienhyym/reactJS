ae607179aba3cf3f8ed132d151a2b4a2e7b22a10
Nguyen Dinh Kien <hydinhkien@gmail.com>
2026-03-27 00:48:37 +0700

    update

--- src/pages/ExtendPage/ExtendCard/ExtendCard.jsx
+++ src/pages/ExtendPage/ExtendCard/ExtendCard.jsx
@@ -3,14 +3,14 @@ import "./ExtendCard.css";
 
 const ExtendCard = ({ extend, onClick }) => {
   return (
-    <div  className="extend-card" >
+    <div className="extend-card" >
       {/* <div className="extend-thumb" onClick={onClick}> ẩn chức năng xem video */}
       <div className="extend-thumb" >
         <img
-          src="https://pub-b41d67fcb1994c9c810548e1c974a2ff.r2.dev/images/chemistry.jpg"
+          src={extend.imageUrl ? extend.imageUrl : "https://pub-b41d67fcb1994c9c810548e1c974a2ff.r2.dev/images/chemistry.jpg"}
           alt={extend.title}
         />
-        {/* <div className="play-overlay">▶</div> ẩn chức năng xem video */} 
+        {/* <div className="play-overlay">▶</div> ẩn chức năng xem video */}
       </div>
       <div className="extend-info" >
 
